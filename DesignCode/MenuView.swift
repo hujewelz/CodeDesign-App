a462703474c4015d8988effd5a25b6f073bd4a69
Jewelz <hujewelz@163.com>
2020-03-03 10:56:07 +0800

    add MenuView

---
+++ DesignCode/MenuView.swift
@@ -0,0 +1,77 @@
+//
+//  MenuView.swift
+//  DesignCode
+//
+//  Created by luobobo on 2020/3/3.
+//  Copyright © 2020 huluobo. All rights reserved.
+//
+
+import SwiftUI
+
+struct MenuView: View {
+    var body: some View {
+        VStack {
+            Spacer()
+            VStack(spacing: 16) {
+                Text("Jewelz - 40% Completed")
+                    .font(.caption)
+                
+                Color.white
+                    .frame(width: 52, height: 6)
+                    .cornerRadius(3)
+                    .frame(width: 130, height: 6, alignment: .leading)
+                    .background(Color.black.opacity(0.08))
+                    .cornerRadius(3)
+                    .padding()
+                    .frame(width: 150, height: 24)
+                    .background(Color.black.opacity(0.1))
+                    .cornerRadius(12)
+                
+                
+                MenuRow(title: "Account", icon: "gear")
+                MenuRow(title: "Billing", icon: "creditcard")
+                MenuRow(title: "Sign out", icon: "person.crop.circle")
+            }
+            .frame(maxWidth: .infinity)
+            .frame(height: 300)
+            .background(LinearGradient(gradient: Gradient(colors: [Color.white, Color(#colorLiteral(red: 0.8705882353, green: 0.8941176471, blue: 0.9450980392, alpha: 1))]), startPoint: .top, endPoint: .bottom))
+            .clipShape(RoundedRectangle(cornerRadius: 30, style: .continuous))
+            .shadow(color: Color.black.opacity(0.2), radius: 20, x: 0, y: 20)
+            .padding(.horizontal, 30)
+            .overlay(
+                Image("Avatar")
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .frame(width: 60, height: 60)
+                    .clipShape(Circle())
+                    .shadow(radius: 10)
+                    .offset(y: -150)
+            )
+        }
+        .padding(.bottom, 30)
+    }
+}
+
+struct MenuView_Previews: PreviewProvider {
+    static var previews: some View {
+        MenuView()
+    }
+}
+
+struct MenuRow: View {
+    var title: String
+    var icon: String
+    
+    var body: some View {
+        HStack(spacing: 16) {
+            Image(systemName: icon)
+                .font(.system(size: 20, weight: .regular, design: .default))
+                .imageScale(.large)
+                .frame(width: 32, height: 32)
+                .foregroundColor(Color(#colorLiteral(red: 0.662745098, green: 0.7333333333, blue: 0.831372549, alpha: 1)))
+            Text(title)
+                .font(.system(size: 20, weight: .bold, design: .rounded))
+                .frame(width: 120, height: 30, alignment: .leading)
+        }
+    }
+}
